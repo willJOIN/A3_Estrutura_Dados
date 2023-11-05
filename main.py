6ebe72fa81bbca9a255a1e8842aaaafe7ce5f1a2
willJOIN <whirano4@gmail.com>
2023-11-05 20:17:20 -0300

    docs: doc que auxilia teste

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
   textos = sorted(
       os.listdir("textos/"),
       key=lambda item: int(item.replace("arq_", "").replace(".txt", "")))
-  tokens_arquivos = Topicos(textos)
+  tokens_arquivos = Topicos(textos) # Para testar 1 arquivo, textos[:1]
   avaliar(tokens_arquivos.topicos_tokens)
