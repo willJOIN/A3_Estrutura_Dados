73cf5644a80b69fc711e6eee304336e02d037ca9
willJOIN <whirano4@gmail.com>
2023-11-10 19:26:19 -0300

    remove: comment teste grafo

--- main.py
+++ main.py
@@ -5,44 +5,6 @@ from grafo import Grafo
 from vertice import Vertice
 
 if __name__ == "__main__":
-
-# # teste
-#   grafo = Grafo()
-#   # criar 11 vertices
-#   grafo.add_vertice('palavra1')
-#   grafo.add_vertice('palavra1')
-  
-#   grafo.add_vertice('palavra2')
-#   grafo.add_vertice('palavra2')
-
-#   grafo.add_vertice('palavra3')
-#   grafo.add_vertice('palavra3')
-  
-#   grafo.add_vertice('palavra4')
-#   grafo.add_vertice('palavra4')
-
-#   grafo.add_vertice('palavra5')
-#   grafo.add_vertice('palavra5')
-
-#   grafo.add_vertice('palavra6')
-#   grafo.add_vertice('palavra6')
-
-#   grafo.add_vertice('palavra7')
-#   grafo.add_vertice('palavra7')
-
-#   grafo.add_vertice('palavra8')
-#   grafo.add_vertice('palavra11')
-
-#   grafo.add_vertice('palavra9')
-#   grafo.add_vertice('palavra9')
-
-#   grafo.add_vertice('palavra10')
-#   grafo.add_vertice('palavra10')
-
-#   grafo.add_vertice('palavra11')
-
-#   print(grafo.get_lista_topicos_importantes())
-
   textos = sorted(
       os.listdir("textos/"),
       key=lambda item: int(item.replace("arq_", "").replace(".txt", "")))
