11d46c37a263d2d0501a1ce19574b99cae9c6dfa
willJOIN <whirano4@gmail.com>
2023-12-05 20:35:03 -0300

    feat: inicia dev similaridade | selecao de atividade main e grafico.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from geral.avaliador import avaliar
 from geral.grafico import plotar_coautoria, plotar_topicos_relevantes
 from geral.tokenization import Tokens
 from modelos.artigo import Artigo
+from geral.util import limpar_terminal
 
 app = Dash(__name__)
 app.title = "A3 Análise e Est. Dados"
@@ -39,14 +40,17 @@ def atividade_coautoria():
     return tokens.grafos[0].get_lista_coautoria()
 
 
+def atividade_similaridade():
+    pass
+
 if __name__ == "__main__":
+    limpar_terminal()
+    print("Atividades A3 - Estrutura de Dados e Análise de Algoritmos:\n")
 
-    # user_input = input('Por favor, insira o número da atividade que você deseja executar:\n1 - Tópicos\n2 - Coautoria')
-    # if '1' in user_input:
-    #   atividade_topicos_relevantes()
-    # elif '2' in user_input:
-    #   atividade_coautoria()
-
-    # plotar_coautoria(app, atividade_coautoria())
-    
-    plotar_topicos_relevantes(app, atividade_topicos_relevantes())
+    user_input = input(
+        "Por favor, insira o número da atividade que você deseja executar:\n\n1 - Tópicos\n2 - Coautoria\n\n"
+    )
+    if "1" in user_input:
+        plotar_topicos_relevantes(app, atividade_topicos_relevantes())
+    elif "2" in user_input:
+        plotar_coautoria(app, atividade_coautoria())
